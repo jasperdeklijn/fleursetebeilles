966e71dbdda950cc764a79c33d2c149616b4288a
jasperdeklijn <54976517+jasperdeklijn@users.noreply.github.com>
2025-09-30 20:07:33 +0200

    Updated page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -45,8 +45,8 @@ export default function Page() {
 
     try {
       const { error } = await supabase.auth.signUp({
-        email: "jasperdeklijn@gmail.com",
-        password: "admintest123",
+        email: "",
+        password: "",
         options: {
           emailRedirectTo: process.env.NEXT_PUBLIC_DEV_SUPABASE_REDIRECT_URL || `${window.location.origin}/admin`,
         },
